326666198c58576bac1ce5e4ed319516af321e43
Jasur <103939215+Jasur7855@users.noreply.github.com>
2024-03-09 15:12:46 +0500

    commit

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-let value =document.querySelectorAll('.numbers')
+let value1 = document.querySelectorAll('.numbers')[0];
+let value2 = document.querySelectorAll('.numbers')[1];
+let value3 = document.querySelectorAll('.numbers')[2];
 
-value.forEach((value)=>{
-    let startValue = 0;
-    let endValue = value.getAttribute('data-val');
-    let result = setInterval(function(){
-        startValue +=5;
-        value.textContent = startValue
+function stepResult(values, step) {
+    values.forEach((value) => {
+        let startValue = 0;
+        let endValue = parseInt(value.getAttribute('data-val')); 
+        let result = setInterval(function() {
+            startValue += step;
+            value.textContent = startValue;
 
-        if(startValue == endValue){
-            clearInterval(result);
-        }
-    })
+            if (startValue >= endValue) { 
+                clearInterval(result);
+            }
+        },100);
+    });
+}
 
-}) 
+stepResult([value1], 500); 
+stepResult([value2], 1);
+stepResult([value3], 100);
